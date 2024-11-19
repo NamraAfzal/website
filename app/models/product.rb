936d52b202c3f45623c32578a2aa3e0de4660954
NamraAfzal <namra.afzal@xprolabs.com>
2024-11-19 18:49:30 +0500

    modified product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   paginates_per 5
   belongs_to :seller
   belongs_to :category
+  has_many :order_items
+  has_many :orders, through: :order_items
 
   validates :name, :price, presence: true
 end
