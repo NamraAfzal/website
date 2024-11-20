12871138ff9988295164d06167ae395fe727c731
NamraAfzal <namra.afzal@xprolabs.com>
2024-11-20 12:48:33 +0500

    Modified model for order to view.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,14 +1,10 @@
 class Order < ApplicationRecord
+  enum status: { cart: 0,placed: 1,confirmed: 2,shipped: 3 }
   belongs_to :user
   has_many :order_items, dependent: :destroy
   has_many :products, through: :order_items
 
-  enum status: {
-    cart: 0,
-    placed: 1,
-    confirmed: 2,
-    shipped: 3
-  }
+
 
   validates :status, presence: true
 
