92969ae85465d769eca2db9b9700506b7e86437b
NamraAfzal <namra.afzal@xprolabs.com>
2024-11-20 11:20:02 +0500

    Modefied models according to the feedback.

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,5 +1,6 @@
 class Seller < ApplicationRecord
   has_many :products, dependent: :destroy
+  has_many :orders
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
