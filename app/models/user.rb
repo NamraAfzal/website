78016f3a941354e1479b9c80267568b11b7abff8
NamraAfzal <namra.afzal@xprolabs.com>
2024-11-20 16:00:31 +0500

    Incorporated the given feedback on models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,5 @@ class User < ApplicationRecord
         errors.add :password, 'must be 10 characters or less and include at least one special character (!@#$%^&*).'
       end
   end
-  def initialize_cart
-    self.create_cart
-  end
+
 end
