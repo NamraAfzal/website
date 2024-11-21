797f33b77cdf44dc5ea89c504f07d879d4064673
NamraAfzal <namra.afzal@xprolabs.com>
2024-11-21 19:08:32 +0500

    modified model for test cases.

--- app/models/seller.rb
+++ app/models/seller.rb
@@ -1,6 +1,5 @@
 class Seller < ApplicationRecord
   has_many :products, dependent: :destroy
-  has_many :orders
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
