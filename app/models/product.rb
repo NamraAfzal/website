e22e29142a3c2712c7976fce522f8bfd407e1a06
NamraAfzal <namra.afzal@xprolabs.com>
2024-12-06 16:16:01 +0500

    Modified modles for validationa and search orders.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,20 @@
 class Product < ApplicationRecord
   paginates_per 5
-  has_many_attached :images
-  validate :validate_images_count
+  has_many_attached :image
   belongs_to :seller
   belongs_to :category
   has_many :order_items
   has_many :orders, through: :order_items
 
-  validates :name, :price, presence: true
+  validates :name, :price, :description, presence: true
+  validates :image, presence: { message: "must be attached to the product" }
   validates :stock, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  private
+  def self.ransackable_attributes(auth_object = nil)
+    %w[id name price created_at updated_at]
+  end
 
-  def validate_images_count
-    if images.count > 5
-      errors.add(:images, "cannot have more than 5 images.")
-    end
+  def total_sales
+    order_items.joins(:order).where(orders: { status: :placed }).sum("order_items.quantity")
   end
 end
