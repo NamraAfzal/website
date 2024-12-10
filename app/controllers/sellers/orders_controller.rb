abc021dee02343ffe41b7fa108d0bece2292f00a
NamraAfzal <namra.afzal@xprolabs.com>
2024-12-10 16:31:06 +0500

    Resolved Merged Conflicts and incorporated given feedback.

--- app/controllers/sellers/orders_controller.rb
+++ app/controllers/sellers/orders_controller.rb
@@ -3,12 +3,12 @@ module Sellers
     before_action :set_order, only: %i[show edit update]
 
     def index
-      @q = Order.for_seller(current_seller).ransack(params[:q])
+      @q = Order.for_seller(current_seller).order(created_at: :desc).ransack(params[:q])
       @orders = @q.result(distinct: true)
     end
 
     def show
-      @orders = Order.for_seller(current_seller)
+      @orders = Order.for_seller(current_seller).order(created_at: :desc)
       @selected_order = @orders.find_by(id: params[:id])
       @q = Order.ransack(params[:q])
       @selected_order = @order
