617de7c2a88c544cf5dc1a4823d960f0316b5123
NamraAfzal <namra.afzal@xprolabs.com>
2024-12-23 17:45:23 +0500

    Modified dashboard controller api to show image url with serializers.

--- app/controllers/api/dashboard_controller.rb
+++ app/controllers/api/dashboard_controller.rb
@@ -5,6 +5,12 @@ class Api::DashboardController < Api::Sellers::BaseController
     product_count = OrderItem.joins(:product).where(products: { seller_id: current_seller.id })
     @total_orders = product_count.count
     @total_sales = product_count.sum("order_items.quantity * products.price")
-    render json: { products: @products, total_products: @total_products, total_orders: @total_orders, total_sales: @total_sales }, status: :ok
+
+    render json: {
+      products: ActiveModelSerializers::SerializableResource.new(@products, each_serializer: ProductSerializer),
+      total_products: @total_products,
+      total_orders: @total_orders,
+      total_sales: @total_sales
+    }, status: :ok
   end
 end
