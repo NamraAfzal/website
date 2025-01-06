fa5e5758da643c92db4f8f5381a2db5869ec46d2
NamraAfzal <namra.afzal@xprolabs.com>
2025-01-06 13:44:33 +0500

    Edited cap files.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-server "13.233.174.62", user: "ubuntu", roles: %w{app db web}
+server "51.20.55.43", user: "ubuntu", roles: %w{app db web}
 
 
 
