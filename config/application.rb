4e79685b091ec2ba23364af7d5a56a570e55085a
NamraAfzal <namra.afzal@xprolabs.com>
2025-01-13 17:23:52 +0500

    upgraded files.

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Myapp
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -22,6 +22,6 @@ module Myapp
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    config.eager_load_paths << Rails.root.join("app/services")
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
